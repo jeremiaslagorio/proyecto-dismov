7552ecc79cc41e72d80f110879d48f24a1947cb6
Julián Unrrein <junrrein@gmail.com>
2020-01-02 19:42:38 -0300

    BaseDatosRemota: Convertir existeNodo para usar Task

--- app/src/main/java/com/junrrein/proyectofinal/BaseDatosRemota.java
+++ app/src/main/java/com/junrrein/proyectofinal/BaseDatosRemota.java
@@ -2,48 +2,43 @@ package com.junrrein.proyectofinal;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.function.Consumer;
-
 public class BaseDatosRemota {
 
     private static final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
     private static final String nodoUsuarios = "usuarios";
 
-    static private void existeNodoAsync(String nodo, Consumer<Boolean> consumidor) {
+    static private Task<Boolean> existeNodo(String nodo) {
+        TaskCompletionSource<Boolean> taskCompletionSource = new TaskCompletionSource<>();
+
         database.child(nodo).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                consumidor.accept(dataSnapshot.exists());
+                taskCompletionSource.setResult(dataSnapshot.exists());
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
+                taskCompletionSource.setException(databaseError.toException());
             }
         });
+
+        return taskCompletionSource.getTask();
     }
 
-    static void existeUsuario(String id, Consumer<Boolean> consumidor) {
-        existeNodoAsync(nodoUsuarios + "/" + id, consumidor);
+    static Task<Boolean> existeUsuario(String id) {
+        return existeNodo(nodoUsuarios + "/" + id);
     }
 
-    static void crearUsuario(String id, Usuario usuario) {
-        existeUsuario(id, existe -> {
-            if (!existe)
-                database.child(nodoUsuarios).child(id).setValue(usuario);
-        });
-    }
-
-    static void actualizarUsuario(String id, Usuario usuario) {
-        existeUsuario(id, existe -> {
-            if (existe)
-                database.child(nodoUsuarios).child(id).setValue(usuario);
-        });
+    static void setearUsuario(String id, Usuario usuario) {
+        database.child(nodoUsuarios).child(id).setValue(usuario);
     }
 
     static void eliminarUsuario(String id) {
