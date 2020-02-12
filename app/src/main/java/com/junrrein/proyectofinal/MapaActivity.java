4383375cba87f689a471ee7afe500c9863b50305
Julián Unrrein <junrrein@gmail.com>
2020-02-12 17:07:18 -0300

    Refactorizar MapaActivity

--- app/src/main/java/com/junrrein/proyectofinal/MapaActivity.java
+++ app/src/main/java/com/junrrein/proyectofinal/MapaActivity.java
@@ -9,11 +9,14 @@ import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 
 public class MapaActivity extends AppCompatActivity {
 
     MapView mapView = null;
+    MapboxMap mapboxMap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +26,22 @@ public class MapaActivity extends AppCompatActivity {
 
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(mapboxMap ->
-                mapboxMap.setStyle(Style.MAPBOX_STREETS, style ->
-                {
-                    double latitud = -31.634788;
-                    double longitud = -60.705824;
-                    CameraPosition position = new CameraPosition.Builder()
-                            .target(new LatLng(latitud, longitud))
-                            .zoom(12)
-                            .build();
-                    mapboxMap.setCameraPosition(position);
-                }));
+        mapView.getMapAsync(onMapReadyCallback);
     }
+
+    private OnMapReadyCallback onMapReadyCallback = mapboxMap -> {
+        this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(Style.MAPBOX_STREETS, this.onStyleLoaded);
+    };
+
+    private Style.OnStyleLoaded onStyleLoaded = style -> {
+        double latitud = -31.634788;
+        double longitud = -60.705824;
+        CameraPosition position = new CameraPosition.Builder()
+                .target(new LatLng(latitud, longitud))
+                .build();
+        mapboxMap.setCameraPosition(position);
+    };
 
     @Override
     protected void onStart() {
