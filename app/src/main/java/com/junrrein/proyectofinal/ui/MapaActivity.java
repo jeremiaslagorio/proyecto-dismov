d7cc88522872c032fa7b5345ec01c391855eae76
Julián Unrrein <junrrein@gmail.com>
2020-03-02 20:22:22 -0300

    MapaActivity: Mostrar ventanas de información cuando se toca en los marcadores de evento

--- app/src/main/java/com/junrrein/proyectofinal/ui/MapaActivity.java
+++ app/src/main/java/com/junrrein/proyectofinal/ui/MapaActivity.java
@@ -1,31 +1,45 @@
 package com.junrrein.proyectofinal.ui;
 
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.junrrein.proyectofinal.R;
+import com.junrrein.proyectofinal.databinding.BurbujaMapaBinding;
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.BubbleLayout;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.Property;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import static com.mapbox.mapboxsdk.style.expressions.Expression.eq;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.get;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.literal;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAnchor;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
 
 public class MapaActivity extends AppCompatActivity {
 
@@ -34,6 +48,11 @@ public class MapaActivity extends AppCompatActivity {
     private static final String SOURCE_ID = "mi.fuente";
     private static final String ICON_ID = "mi.icono";
     private static final String MARKER_LAYER_ID = "mi.capa.marcadores";
+    private static final String BURBUJA_LAYER_ID = "mi.capa.burbujas";
+    private static final String PROPERTY_ID = "id";
+    private static final String PROPERTY_TITULO = "titulo";
+    private static final String PROPERTY_DESCRIPCION = "descripcion";
+    private static final String PROPERTY_SELECCIONADO = "seleccionado";
 
     private MapView mapView;
     private MapboxMap mapboxMap;
@@ -64,24 +83,32 @@ public class MapaActivity extends AppCompatActivity {
     };
 
     private Style.OnStyleLoaded onStyleLoaded = style -> {
-        double latitud = -31.634788;
-        double longitud = -60.705824;
-        CameraPosition position = new CameraPosition.Builder()
-                .target(new LatLng(latitud, longitud))
-                .zoom(12.0)
-                .build();
-        mapboxMap.setCameraPosition(position);
-
         style.addSource(source);
         style.addImage(ICON_ID, BitmapFactory.decodeResource(getResources(), R.drawable.mapbox_marker_icon_default));
         style.addLayer(generarMarkerLayer());
+        style.addImages(generarBurbujas());
+        style.addLayer(generarBurbujaLayer());
+
+        mapboxMap.setCameraPosition(determinarPosicionDeCamaraInicial());
+        mapboxMap.addOnMapClickListener(this.onMapClickListener);
     };
+
+    private CameraPosition determinarPosicionDeCamaraInicial() {
+        return new CameraPosition.Builder()
+                .target(new LatLng(-31.634788, -60.705824))
+                .zoom(12.0)
+                .build();
+    }
 
     private FeatureCollection generarFeatures() {
         List<Feature> features = new ArrayList<>();
 
         for (EventoMapa evento : eventos) {
             Feature feature = Feature.fromGeometry(Point.fromLngLat(evento.longitud, evento.latitud));
+            feature.addStringProperty(PROPERTY_ID, evento.id);
+            feature.addStringProperty(PROPERTY_TITULO, evento.nombre);
+            feature.addStringProperty(PROPERTY_DESCRIPCION, evento.fecha.toString());
+            feature.addBooleanProperty(PROPERTY_SELECCIONADO, false);
 
             features.add(feature);
         }
@@ -94,6 +121,86 @@ public class MapaActivity extends AppCompatActivity {
                 .withProperties(iconImage(ICON_ID),
                         iconAllowOverlap(true),
                         iconIgnorePlacement(true));
+    }
+
+    private HashMap<String, Bitmap> generarBurbujas() {
+        HashMap<String, Bitmap> result = new HashMap<>();
+        assert (featureCollection.features() != null);
+
+        for (Feature feature : featureCollection.features()) {
+            BurbujaMapaBinding binding = BurbujaMapaBinding.inflate(getLayoutInflater());
+            @SuppressWarnings("deprecation") BubbleLayout bubbleLayout = (BubbleLayout) binding.getRoot();
+
+            binding.infoWindowTitle.setText(feature.getStringProperty(PROPERTY_TITULO));
+            binding.infoWindowDescription.setText(feature.getStringProperty(PROPERTY_DESCRIPCION));
+
+            int measureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+            bubbleLayout.measure(measureSpec, measureSpec);
+
+            float measuredWidth = bubbleLayout.getMeasuredWidth();
+            bubbleLayout.setArrowPosition(measuredWidth / 2 - 5);
+
+            Bitmap bitmap = generarBitmapDeView(bubbleLayout);
+            result.put(feature.getStringProperty(PROPERTY_ID), bitmap);
+        }
+
+        return result;
+    }
+
+    private static Bitmap generarBitmapDeView(@NonNull View view) {
+        int measureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        view.measure(measureSpec, measureSpec);
+
+        int measuredWidth = view.getMeasuredWidth();
+        int measuredHeight = view.getMeasuredHeight();
+
+        view.layout(0, 0, measuredWidth, measuredHeight);
+        Bitmap bitmap = Bitmap.createBitmap(measuredWidth, measuredHeight, Bitmap.Config.ARGB_8888);
+        bitmap.eraseColor(Color.TRANSPARENT);
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+
+        return bitmap;
+    }
+
+    private SymbolLayer generarBurbujaLayer() {
+        return new SymbolLayer(BURBUJA_LAYER_ID, SOURCE_ID)
+                .withProperties(
+                        iconImage("{id}"),
+                        iconAnchor(Property.ICON_ANCHOR_BOTTOM),
+                        iconAllowOverlap(true),
+                        iconOffset(new Float[]{-2f, -30f}))
+                .withFilter(eq(get(PROPERTY_SELECCIONADO), literal(true)));
+    }
+
+    private MapboxMap.OnMapClickListener onMapClickListener = point -> {
+        PointF screenPoint = mapboxMap.getProjection().toScreenLocation(point);
+        List<Feature> marcadoresClickeados = mapboxMap.queryRenderedFeatures(screenPoint, MARKER_LAYER_ID);
+
+        if (marcadoresClickeados.isEmpty())
+            return false;
+
+        Feature marcadorClickeado = marcadoresClickeados.get(0);
+        String idMarcadorClickeado = marcadorClickeado.getStringProperty(PROPERTY_ID);
+        assert (featureCollection.features() != null);
+
+        for (Feature feature : featureCollection.features()) {
+            String idFeature = feature.getStringProperty(PROPERTY_ID);
+
+            if (idMarcadorClickeado.equals(idFeature)) {
+                boolean estaSeleccionado = feature.getBooleanProperty(PROPERTY_SELECCIONADO);
+                feature.addBooleanProperty(PROPERTY_SELECCIONADO, !estaSeleccionado);
+                refrescarSource();
+
+                break;
+            }
+        }
+
+        return true;
+    };
+
+    private void refrescarSource() {
+        source.setGeoJson(featureCollection);
     }
 
     @Override
